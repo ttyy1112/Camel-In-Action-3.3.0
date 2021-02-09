b944389ce04fa1db3f82378650a4fc2d3374fef9
tanghonggang1 <tanghonggang1@jd.com>
2021-02-09 15:05:35 +0800

    error handler

---
+++ java-app/error-handler/src/test/java/com/atang/camel/OrderException.java
@@ -0,0 +1,10 @@
+package com.atang.camel;
+
+public class OrderException extends Exception {
+    
+	private static final long serialVersionUID = -2573258934191495202L;
+
+	public OrderException(String s) {
+        super(s);
+    }
+}
